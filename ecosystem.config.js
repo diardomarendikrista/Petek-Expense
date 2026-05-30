71d6ade2fd51ca218eed013441484989be723275
Diardo Marendi Krista <diardomarendikrista@gmail.com>
2026-05-30 14:10:44 +0700

    fix: explicit node interpreter in ecosystem to stop windows cmd flashing

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,16 +2,16 @@ module.exports = {
   apps: [
     {
       name: "petek-expense",
-      script: "node_modules/next/dist/bin/next",
-      args: "start",
+      script: "node",
+      args: "node_modules/next/dist/bin/next start",
       env: {
         NODE_ENV: "production",
       },
     },
     {
       name: "petek-expense-backup",
-      script: "node_modules/tsx/dist/cli.mjs",
-      args: "scripts/backup.ts",
+      script: "node",
+      args: "node_modules/tsx/dist/cli.mjs scripts/backup.ts",
     },
   ],
 };
